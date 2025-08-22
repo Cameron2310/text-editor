be6757bb1d795b5a1ef6bfbc12f1adbd890dca63
Cameron2310 <wilke7566@gmail.com>
2025-08-22 16:30:43 -0400

    Updated test

--- tests/test_many_lines.sh
+++ tests/test_many_lines.sh
@@ -1,3 +1,55 @@
 for i in {0..5000}; do
-  echo "This is line $i" >> ./output.txt
+    echo -n "This is line $i" >> ./output.txt
+    for j in {0..100}; do
+        echo -n "$j " >> output.txt
+    done
+
+    echo "" >> output.txt
 done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
